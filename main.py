1a039180c224c7fb0338d99439eab13167231f9c
Alex62-Creator <cham62ss@gmail.com>
2025-01-06 11:29:47 +0300

    add move and score

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+
 pygame.init()
 
 SCREEN_WIDTH = 800
@@ -18,20 +19,32 @@ target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
 color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+font = pygame.font.Font(None, 24)
+text = ""
 
-
+shot = 0
+result = 0
 running = True
 while running:
     screen.fill(color)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
+            shot += 1
             mouse_x, mouse_y = pygame.mouse.get_pos()
             if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
-                target_x = random.randint(0, SCREEN_WIDTH - target_width)
-                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+                result += 1
+
+    target_x = random.randint(0, SCREEN_WIDTH - target_width)
+    target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
     screen.blit(target_img, (target_x, target_y))
+    text = font.render(f'Результат {result} из {shot}', True, (0, 0, 0))
+    screen.blit(text, (600, 580))
+    
     pygame.display.update()
+    pygame.time.delay(1000)
 
 pygame.quit()
